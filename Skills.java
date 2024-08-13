92f939d75cf8ed66eaac8a8c01e9fd5afe568c5f
shivani-vorukolu <96616839+shivani-vorukolu@users.noreply.github.com>
2024-08-13 10:45:05 +0530

    Update Skills.java

--- Skills.java
+++ Skills.java
@@ -4,16 +4,6 @@ class Skills
 {
 	public static void main(String args[])
 	{
-		List<String> skills = new ArrayList<>();
-		skills.add("Java");
-		skills.add("python");
-		skills.add("C");
-		skills.add("HTML CSS");
-		skills.add("Operating System");
-
-		for(int i = 0; i <= skills.size(); i++)
-		{
-			System.out.println(skills.get(i));
-		}
+		System.out.println("Hello World");
 	}
 }
